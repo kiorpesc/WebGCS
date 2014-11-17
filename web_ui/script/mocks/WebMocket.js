be7cfe8ec8c27050407ee454e4075c86fa7e52df
kiorpesc <ckiorpes@gmail.com>
2014-11-17 23:25:37 +0000

    More tests

--- web_ui/script/mocks/WebMocket.js
+++ web_ui/script/mocks/WebMocket.js
@@ -43,7 +43,7 @@ WebMocket.prototype.close = function() {
 
 WebMocket.prototype.generateHeartbeat = function () {
     
-    JSON.stringify(this.heartbeat);
+    console.log(this.heartbeat);
     
     setTimeout(this.generateHeartbeat, 1000);
 }
