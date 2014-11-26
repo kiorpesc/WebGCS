8048116a9399ec98a6cf96847091b8a719e3b6e8
kiorpesc <ckiorpes@gmail.com>
2014-11-26 21:56:49 +0000

    Typo

--- web_ui/tests/features/step_definitions/myStepDefinitions.js
+++ web_ui/tests/features/step_definitions/myStepDefinitions.js
@@ -24,7 +24,7 @@ var myStepDefinitionsWrapper = function () {
   this.When(/^I click the Flight Modes button$/, function(callback) {
     // Express the regexp above with the code you wish you had. Call callback() at the end
     // of the step, or callback.pending() if the step is not yet implemented:
-    this.clickLink("Flight Modes", callback);
+    this.clickLink("Flight Mode", callback);
   });
 
   this.Then(/^I should see "(.*)" as the page title$/, function(title, callback) {
