f76c765c9968644d810d8540627995f98619cad6
Charles Kiorpes <ckiorpes@gmail.com>
2014-11-26 18:29:53 -0500

    Bring browser into scope

--- web_ui/tests/features/step_definitions/myStepDefinitions.js
+++ web_ui/tests/features/step_definitions/myStepDefinitions.js
@@ -1,6 +1,6 @@
 var myStepDefinitionsWrapper = function () {
   this.World = require("../support/world.js").World; // overwrite default World constructor
-  var browser = this.World.browser;
+  this.browser = this.World.browser;
     
   var assert = require('assert');
     
@@ -9,14 +9,14 @@ var myStepDefinitionsWrapper = function () {
     // `this` is set to a new this.World instance.
     // i.e. you may use this.browser to execute the step:
 
-      browser.visit('http://fly.quadworkshop.com', callback);
+      this.browser.visit('http://fly.quadworkshop.com', callback);
 
     // The callback is passed to visit() so that when the job's finished, the next step can
     // be executed by Cucumber.
   });
 
   this.Given(/^There are no UAVs connected$/, function(callback) {
-      var nUAVs = this.evaluate("window.uavs.getNumUAVs()");
+      var nUAVs = this.browser.evaluate("window.uavs.getNumUAVs()");
       if (nUAVs === 0) {
           callback();
       } else {
@@ -27,11 +27,11 @@ var myStepDefinitionsWrapper = function () {
   this.When(/^I click the Flight Modes button$/, function(callback) {
       // Express the regexp above with the code you wish you had. Call callback() at the end
       // of the step, or callback.pending() if the step is not yet implemented:
-      this.clickLink("#flight-mode-link", callback);
+      this.browser.clickLink("#flight-mode-link", callback);
   });
 
   this.Then(/^I should not see any flight modes$/, function(callback) {
-      var modes = this.html("#flight-modes");
+      var modes = this.browser.html("#flight-modes");
   
       if (modes === ""){
           callback();
@@ -41,7 +41,8 @@ var myStepDefinitionsWrapper = function () {
   });  
   
   this.Given(/^I connect a single UAV$/, function(callback) {
-      this.clickLink("#add-uav-link", function() {
+      var browser = this.browser;
+      browser.clickLink("#add-uav-link", function() {
           browser.fill("#uav_ip", "sw-testing").pressButton("#submit-uav", callback);
       })
   });  
