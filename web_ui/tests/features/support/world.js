acb2743e66374b997ec4e8deb8a6ef8228dcee0c
kiorpesc <ckiorpes@gmail.com>
2014-11-27 13:24:38 +0000

    Remove unneeded world defs

--- web_ui/tests/features/support/world.js
+++ web_ui/tests/features/support/world.js
@@ -4,19 +4,6 @@ var WorldConstructor = function WorldConstructor(callback) {
 
   var world = {
     browser: this.browser,
-/*    visit: function(url, callback) {
-      this.browser.visit(url, callback);
-    }.bind(this),
-    evaluate: function(js_str) {
-        return this.browser.evaluate(js_str);  
-    }.bind(this),
-    clickLink: function(link_str, callback) {
-        this.browser.clickLink(link_str, callback);
-    }.bind(this),
-    html: function(selector, context) {
-        return this.browser.html(selector, context);
-    }.bind(this),
-*/
   };
 
   callback(world); // tell Cucumber we're finished and to use our world object instead of 'this'
