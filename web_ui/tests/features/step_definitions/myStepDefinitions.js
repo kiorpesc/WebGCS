9bf94a2011438f6cb3ed32117bf56d86c89da7a6
Charles Kiorpes <ckiorpes@gmail.com>
2014-11-27 11:21:05 -0500

    Some buggy file doubling going on

--- web_ui/tests/features/step_definitions/myStepDefinitions.js
+++ web_ui/tests/features/step_definitions/myStepDefinitions.js
@@ -40,95 +40,13 @@ var myStepDefinitionsWrapper = function () {
       }
   });  
  
-  this.addedOneUAV = function() {
-    return this.browser.evaluate("window.uavs.getNumUAVs()") === 1;
-  }
   
-  // right now this causes zombie to lose window context
   this.Given(/^I connect a single UAV$/, function(callback) {
       console.log(this.browser.window.uavs);
 
+      // right now this causes zombie to lose window context
       this.browser.fill("#uav_ip", "sw-testing").pressButton("#submit-uav");
 
-      //this.browser.wait(this.addedOneUAV, function() {console.log(this.browser.window)});
-      
-      //var nUAVs = this.browser.evaluate("window.uavs.getNumUAVs()");
-
-      console.log(this.browser.location);
-      
-      //assert.equal(nUAVs, 1);
-      callback.pending();
-  });  
-    
-  this.Then(/^I should see "(.*)" as the page title$/, function(title, callback) {
-    // matching groups are passed as parameters to the step definition
-
-    var pageTitle = this.browser.text('title');
-    if (title === pageTitle) {
-      callback();
-    } else {
-      callback.fail(new Error("Expected to be on page with title " + title));
-    }
-  });
-};
-
-module.exports = myStepDefinitionsWrapper;
-var myStepDefinitionsWrapper = function () {
-  this.World = require("../support/world.js").World; // overwrite default World constructor
-  this.browser = this.World.browser;
-    
-  var assert = require('assert');
-    
-  this.Given(/^I am viewing the app$/, function(callback) {
-    // Express the regexp above with the code you wish you had.
-    // `this` is set to a new this.World instance.
-    // i.e. you may use this.browser to execute the step:
-      //this.browser.close();
-      this.browser.visit('http://fly.quadworkshop.com', callback);
-
-    // The callback is passed to visit() so that when the job's finished, the next step can
-    // be executed by Cucumber.
-  });
-
-  this.Given(/^There are no UAVs connected$/, function(callback) {
-      var nUAVs = this.browser.evaluate("window.uavs.getNumUAVs()");
-      if (nUAVs === 0) {
-          callback();
-      } else {
-          callback.fail(new Error("Number of UAVs - Expected: 0, Actual: " + nUAVs));
-      }
-  });
-    
-  this.When(/^I click the Flight Modes button$/, function(callback) {
-      // Express the regexp above with the code you wish you had. Call callback() at the end
-      // of the step, or callback.pending() if the step is not yet implemented:
-      this.browser.clickLink("#flight-mode-link", callback);
-  });
-
-  this.Then(/^I should not see any flight modes$/, function(callback) {
-      var modes = this.browser.html("#flight-modes");
-  
-      if (modes === ""){
-          callback();
-      } else {
-          callback.fail(new Error("Flight mode div not empty."));
-      }
-  });  
- 
-  this.addedOneUAV = function() {
-    return this.browser.evaluate("window.uavs.getNumUAVs()") === 1;
-  }
- 
-  this.Given(/^I connect a single UAV$/, function(callback) {
-      console.log(this.browser.window.uavs);
-
-      this.browser.fill("#uav_ip", "sw-testing").pressButton("#submit-uav");
-
-      this.browser.wait(this.addedOneUAV, function() {console.log(this.browser.window)});
-      
-      var nUAVs = this.browser.evaluate("window.uavs.getNumUAVs()");
-
-      assert.equal(nUAVs, 1);
       callback.pending();
   });  
     
